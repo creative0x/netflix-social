8c2308252e2dc2ca22a3baa8351a9904e11277d1
creative0x <creative0x.proton.me>
2022-10-13 21:37:32 -0600

    social input area complete

--- components/social/Input.js
+++ components/social/Input.js
@@ -1,6 +1,35 @@
-import React from "react";
+import {
+  CalendarIcon,
+  ChartBarIcon,
+  EmojiHappyIcon,
+  PhotographIcon,
+  XIcon,
+} from "@heroicons/react/outline";
+
+import data from "@emoji-mart/data";
+import Picker from "@emoji-mart/react";
+import React, { useRef, useState } from "react";
 
 export default function Input() {
+  const [input, setInput] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [showEmojis, setShowEmojis] = useState(false);
+
+  // Providing a reference to another element
+  const filePickerRef = useRef(null);
+
+  // adds emoji into input field
+  const addEmoji = (e) => {
+    let sym = e.unified.split("-");
+    let codesArray = [];
+    sym.forEach((el) => codesArray.push("0x" + el));
+    let emoji = String.fromCodePoint(...codesArray);
+    setInput(input + emoji);
+  };
+
+  const addImageToPost = () => {};
+
+  const sendPost = () => {};
   return (
     <div
       className={`border-b border-gray-700 p-3 flex space-x-3 overflow-y-scroll`}
@@ -10,7 +39,82 @@ export default function Input() {
         alt=""
         className="h-11 w-11 rounded-full cursor-pointer"
       />
-      <div></div>
+
+      <div className="w-full divide-y divide-gray-700">
+        {/* create some space for text area after image is loaded to input area */}
+        <div className={`${selectedFile && "pb-7"} ${input && "space-y-2.5"}`}>
+          {/* onChange allows to set the input */}
+          <textarea
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            rows="2"
+            placeholder="What's happening?"
+            className="bg-transparent outline-none text-[#d9d9d9] text-lg placeholder-gray-500 tracking-wide w-full min-h-[50px]"
+          />
+          {/* Only display X icon if file is imported */}
+          {selectedFile && (
+            <div className="relative">
+              <div
+                className="absolute w-8 h-8 bg-[#15181c] hover:bg-[#272c26] bg-opacity-75 rounded-full flex items-center justify-center top-1 left-1 cursor-pointer"
+                onClick={() => setSelectedFile(null)}
+              >
+                <XIcon className="text-white h-5" />
+              </div>
+              <img
+                src={selectedFile}
+                alt=""
+                className="rounded-2xl max-h-80 object-contain"
+              />
+            </div>
+          )}
+        </div>
+
+        <div className="flex items-center justify-between pt-2.5">
+          <div className="flex items-center">
+            {/* on click run the file input on the photograph icon */}
+            <div className="icon" onClick={() => filePickerRef.current.click()}>
+              <PhotographIcon className="h-[22px] text-[#E20910]" />
+              <input
+                type="file"
+                hidden
+                onChange={addImageToPost}
+                ref={filePickerRef}
+              />
+            </div>
+            <div className="icon rotate-90">
+              <ChartBarIcon className="text-[#E20910] h-[22px]" />
+            </div>
+
+            <div className="icon" onClick={() => setShowEmojis(!showEmojis)}>
+              <EmojiHappyIcon className="text-[#E20910] h-[22px]" />
+            </div>
+
+            <div className="icon">
+              <CalendarIcon className="text-[#E20910] h-[22px]" />
+            </div>
+            {showEmojis && (
+              <div
+                style={{
+                  position: "absolute",
+                  marginTop: "465px",
+                  marginLeft: -40,
+                  maxWidth: "120px",
+                  borderRadius: "20px",
+                }}
+              >
+                <Picker data={data} onEmojiSelect={addEmoji} theme="dark" />
+              </div>
+            )}
+          </div>
+          <button
+            className="bg-[#AF0712] text-white rounded-full px-4 py-1.5 font-bold shadow-md hover:bg-[#AF0712] disabled:hover:bg-[#AF0712] disabled:opacity-50 disabled:cursor-default"
+            disabled={!input.trim() && !selectedFile}
+            onClick={sendPost}
+          >
+            Share
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
