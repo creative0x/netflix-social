0bb8dc402e16695ecd88fad79e8bf50f9de07da1
creative0x <creative0x.proton.me>
2022-10-13 20:37:54 -0600

    social feed area complete

---
+++ components/social/Input.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Input() {
+  return (
+    <div
+      className={`border-b border-gray-700 p-3 flex space-x-3 overflow-y-scroll`}
+    >
+      <img
+        src="https://i.stack.imgur.com/34AD2.jpg"
+        alt=""
+        className="h-11 w-11 rounded-full cursor-pointer"
+      />
+      <div></div>
+    </div>
+  );
+}
--- components/social/Sidebar.js
+++ components/social/Sidebar.js
@@ -14,7 +14,7 @@ import {
 
 function Sidebar() {
   return (
-    <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full">
+    <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full mt-5">
       <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24">
         <Image
           src="https://www.edigitalagency.com.au/wp-content/uploads/Netflix-N-Symbol-logo-red-transparent-RGB-png.png"
