a840177e9ae2962f9bd79400aa754d1c0f348049
creative0x <creative0x.proton.me>
2022-10-16 00:54:11 -0600

    get, delete, like, fire  post completed

--- components/social/Feed.js
+++ components/social/Feed.js
@@ -1,10 +1,28 @@
 import { SparklesIcon } from "@heroicons/react/outline";
-import React from "react";
+import { useEffect, useState } from "react";
 import Input from "./Input";
+import { onSnapshot, collection, query, orderBy } from "@firebase/firestore";
+import { db } from "../../firebase";
+import Post from "./Post";
+import { useSession } from "next-auth/react";
 
 export default function Feed() {
-  // flex grow allows for the feed to maintain its size
+  const [posts, setPosts] = useState([]);
+
+  // get all posts from firebase and sort by time
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, "posts"), orderBy("timestamp", "desc")),
+        (snapshot) => {
+          setPosts(snapshot.docs);
+        }
+      ),
+    [db]
+  );
+
   return (
+    // flex grow allows for the feed to maintain its size
     <div className="text-white flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
       <div className="text-[#d9d9d9] flex items-center sm:justify-between py-2 px-3 sticky top-0 z-50 bg-black border-b border-gray-700">
         <h2 className="text-lg sm:text-xl font-bold">Home</h2>
@@ -13,6 +31,12 @@ export default function Feed() {
         </div>
       </div>
       <Input />
+      <div className="pb-72">
+        {posts.map((post) => (
+          // post.data contains the object with everything that was pushed to the db
+          <Post key={post.id} id={post.id} post={post.data()} />
+        ))}
+      </div>
     </div>
   );
 }
