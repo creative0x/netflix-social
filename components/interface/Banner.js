60a16abd48c06e3a1b931a9ac81caa78becef492
creative0x <creative0x.proton.me>
2022-10-18 11:40:37 -0600

    custom scrollbar complete

--- components/interface/Banner.js
+++ components/interface/Banner.js
@@ -13,7 +13,7 @@ export default function Banner({ trendingNow }) {
 
   return (
     <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
-      <div className="absolute top-0 left-0 -z-10 h-[95vh] w-[99vw]">
+      <div className="absolute top-0 left-0 -z-10 h-[95vh] w-[99.5vw]">
         <Image
           src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
           alt=""
--- components/interface/Thumbnail.js
+++ components/interface/Thumbnail.js
@@ -4,16 +4,17 @@ import React from "react";
 export default function Thumbnail({ result }) {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
   return (
-    <div className="cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50 p-5">
+    <div className="cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50 p-6">
       <Image
         layout="responsive"
         src={
-          `${BASE_URL}${result.backdrop_path}` ||
-          `${BASE_URL}${result.poster_path}`
+          `${BASE_URL}${result.poster_path}` ||
+          `${BASE_URL}${result.backdrop_path}`
         }
-        height={1080}
-        width={1920}
+        height={3840}
+        width={2592}
         alt=""
+        className="rounded-sm md:rounded-lg"
       />
     </div>
   );
